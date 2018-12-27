6427cadc1a78cdee02ecf016c9d86bb0f788013d
Amir Dolkhani <amir.dolkhani@gmail.com>
2018-12-27 22:36:54 +0100

    Add Greeter service main class

--- src/com/amirdolkhani/greeter/Greeter.java
+++ src/com/amirdolkhani/greeter/Greeter.java
@@ -1,7 +1,16 @@
 package com.amirdolkhani.greeter;
 
+import com.amirdolkhani.greeter.datasource.UserDataSource;
+import com.amirdolkhani.greeter.entity.User;
+import com.amirdolkhani.greeter.util.GreetingBuilder;
+
 public class Greeter {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		UserDataSource userDataSource = new UserDataSource();		
+		User user = userDataSource.getUser2();
+		GreetingBuilder greetingBuilder = new GreetingBuilder();
+		String message = greetingBuilder.buildGreeting(user);
+		System.out.println(message);
 	}
 }
